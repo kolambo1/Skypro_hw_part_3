764aca637b3f4efb8c47443fdaafd3d0d6d5dd32
Nikolai <dereviankonikolai@gmail.com>
2024-02-26 14:49:23 +0300

    change

--- lesson_3_task_1.py
+++ lesson_3_task_1.py
@@ -1,8 +1,7 @@
 # lesson_3_task_1.py
-from user import User # Импортируем класс User из файла user.py
+from users import User # Импортируем класс User из файла user.py
 
 my_user = User("Иван", "Иванов") # Создаем новый экземпляр User и сохраняем его в переменную my_user
 my_user.print_first_name() # Вызываем метод, печатающий имя
 my_user.print_last_name() # Вызываем метод, печатающий фамилию
 my_user.print_full_name() # Вызываем метод, печатающий имя и фамилию
-#jjffjjffjfj
